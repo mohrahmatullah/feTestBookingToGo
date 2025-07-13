a750e6bb1eee4f443171e49ab71f9c0f255211ea
Rahmat <rahmetuloh@gmail.com>
2025-07-13 11:42:58 +0700

    first commit

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -60,6 +60,7 @@
             </div>
 
             <button type="submit" class="btn btn-success mt-3">Simpan</button>
+            <a href="{{ route('customers.index') }}" class="btn btn-secondary mt-3">Kembali</a>
         </form>
     </div>
 </main>
--- resources/views/customers/edit.blade.php
+++ resources/views/customers/edit.blade.php
@@ -63,7 +63,7 @@
                 @endforeach
             </div>
 
-            <button type="submit" class="btn btn-success mt-3">Simpan Perubahan</button>
+            <button type="submit" class="btn btn-success mt-3">Simpan</button>
             <a href="{{ route('customers.index') }}" class="btn btn-secondary mt-3">Kembali</a>
         </form>
     </div>
